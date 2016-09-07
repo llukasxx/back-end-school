b47ad1d2bb1b1fbddac230cf7f3977df10266ef2
lukasx <llukasxx@gmail.com>
2016-09-07 03:53:27 -0700

    Add simple event resource

--- db/schema.rb
+++ db/schema.rb
@@ -11,10 +11,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160608141433) do
+ActiveRecord::Schema.define(version: 20160907103943) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "events", force: :cascade do |t|
+    t.string   "name"
+    t.datetime "date"
+    t.integer  "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "grades", force: :cascade do |t|
     t.integer  "student_id"
---
+++ spec/factories/events.rb
@@ -0,0 +1,5 @@
+FactoryGirl.define do
+  factory :event do
+    date "2016-09-07 03:39:43"
+  end
+end
