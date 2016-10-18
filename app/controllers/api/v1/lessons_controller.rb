efddab505d2adb5b96018982dda9566965b22bae
lukasx <llukasxx@gmail.com>
2016-10-18 17:04:50 +0200

    Refactor lessons controllers

--- app/controllers/api/v1/lessons_controller.rb
+++ app/controllers/api/v1/lessons_controller.rb
@@ -2,26 +2,17 @@ class Api::V1::LessonsController < ApplicationController
   before_action :authenticate_user_from_token!
 
   def index
-    if lesson_params[:query] && !lesson_params[:query].empty?
-      lessons = Lesson.with_groups.search_by_name(lesson_params[:query]).page(lesson_params[:page])
-      lessons_count = lessons.count
-      render json: lessons, each_serializer: ReceiverLessonSerializer, meta: { count: lessons_count }, status: :ok
-    elsif lesson_params[:student_id]
-      student = User.find(lesson_params[:student_id])
-      lessons = student.student_lessons.includes(:lesson_dates)
-      render json: lessons, each_serializer: SimpleStudentLessonSerializer, scope: current_user, status: :ok
-    else
-      lessons = Lesson.with_groups.page(lesson_params[:page])
-      lessons_count = Lesson.all.count
-      render json: lessons, each_serializer: ReceiverLessonSerializer, meta: { count: lessons_count }, status: :ok
-    end
+    lessons_query = Lesson::LessonQuery.new(student_id: query_params[:student_id])
+    render json: lessons_query.result_query, 
+                 each_serializer: SimpleStudentLessonSerializer,
+                 scope: current_user
   end
 
 
   private
 
-    def lesson_params
-      params.permit(:query, :page, :student_id)
+    def query_params
+      params.permit(:student_id)
     end
 
 end
---
+++ app/models/lesson/lesson_query.rb
@@ -0,0 +1,30 @@
+class Lesson
+  class LessonQuery
+    attr_reader :search_query, :student_id
+    def initialize(search_query: '', student_id: nil)
+      @search_query = search_query
+      @student_id = student_id
+    end
+
+    def result_query
+      filtered
+    end
+
+    def count
+      filtered.count
+    end
+
+    private
+
+      def filtered
+        if(student_id.present?)
+          student = User.find(student_id)
+          student.student_lessons.includes(:lesson_dates)
+        elsif(search_query.present?)
+          Lesson.with_groups.search_by_name(search_query)
+        else
+          Lesson.with_groups.all
+        end
+      end
+  end
+end
